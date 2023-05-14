22b4992cb7b905eaf7e2fe2f33b1f916fc3ed50c
Kristina Jovanovska <kristinajovanovska993@gmail.com>
2023-05-14 21:23:12 +0200

    hello world of php

---
+++ hello_world.php
@@ -0,0 +1,5 @@
+<?php 
+echo "<body style='background-color:A44CD3; margin-left:110px'>";
+echo "<h1 style='font-weight:bold; color:#000080';>Hello php world!</h1>";
+echo "<img src='images\php-logo.png' alt='Image' style='margin-left: 40px';>";
+?>
